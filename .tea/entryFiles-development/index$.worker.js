01956e533d082a44897ec8f4b475e36d5e7dafee
Genius Funny <geniusfunnyyh@gmail.com>
2019-05-30 09:47:21 +0800

    init

---
+++ .tea/entryFiles-development/index$.worker.js
@@ -0,0 +1,24 @@
+if(!self.__appxInited) {
+self.__appxInited = 1;
+
+
+require('./config$');
+
+
+var AFAppX = self.AFAppX;
+self.getCurrentPages = AFAppX.getCurrentPages;
+self.getApp = AFAppX.getApp;
+self.Page = AFAppX.Page;
+self.App = AFAppX.App;
+self.my = AFAppX.bridge || AFAppX.abridge;
+self.abridge = self.my;
+self.Component = AFAppX.WorkerComponent || function(){};
+self.$global = AFAppX.$global;
+
+
+function success() {
+require('../..//app');
+require('../../pages/index/index');
+}
+self.bootstrapApp ? self.bootstrapApp({ success }) : success();
+}
